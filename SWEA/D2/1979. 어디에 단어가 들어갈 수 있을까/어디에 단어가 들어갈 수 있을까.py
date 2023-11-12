0026bf975ca49d2b5d25e1dd7771cc66d66b938c
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2023-11-12 18:47:38 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 148 ms, Memory: 58,528 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -1,25 +1,21 @@
+def count(arr):
+    result = 0
+    for li in arr:
+        count = 0
+        for j in range(len(li)):
+            if li[j]:
+                count += 1
+            else:
+            	if count == k:
+                	result += 1
+            	count = 0
+    return result
+
 T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     n, k = map(int, input().split())
-    li = [list(map(int, input().split())) for _ in range(n)]
-    result = 0
-    for i in range(n):
-        count = 0
-        for j in range(n):
-            if li[i][j] == 1:
-                count += 1
-            if li[i][j] == 0 or j == n - 1:
-                if count == k:
-                    result += 1
-                count = 0
-                
-        for j in range(n):
-            if li[j][i] == 1:
-                count += 1
-            if li[j][i] == 0 or j == n - 1:
-                if count == k:
-                    result += 1
-                count = 0
+    li = [list(map(int, input().split())) + [0] for _ in range(n)] + [[0] * (n+ 1)]
+    li2 = list(zip(*li))
 	
-    print("#{} {}" .format(test_case, result))
+    print("#{} {}" .format(test_case, count(li) + count(li2)))
