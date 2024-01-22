e30ecf44e71964ee47eaaa55ab1227e1c7e5cd36
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2024-01-22 09:04:31 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -2,10 +2,8 @@ import string
 
 def solution(s, skip, index):
     alpa = sorted(set(string.ascii_lowercase) - set(skip))
-    print(alpa)
     result = ""
     for i in s:
         result += alpa[(alpa.index(i) + index) % len(alpa)]
-        
         
     return result
