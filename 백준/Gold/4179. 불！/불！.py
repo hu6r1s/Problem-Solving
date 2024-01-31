db78a22398b8fd91c32c5632265a26c2e63d8349
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2024-02-01 00:03:31 +0900

    [Gold IV] Title: 불!, Time: 1552 ms, Memory: 71468 KB -BaekjoonHub

---
+++ 백준/Gold/4179. 불！/불！.py
@@ -0,0 +1,49 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+r, c = map(int, input().split())
+graph = [list(input().rstrip()) for _ in range(r)]
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+jihoon = deque()
+fire = deque()
+f_time = [[0]*c for _ in range(r)]
+j_time = [[0]*c for _ in range(r)]
+for i in range(r):
+    for j in range(c):
+        if graph[i][j] == "J":
+            jihoon.append([i, j])
+            j_time[i][j] = 1
+        if graph[i][j] == "F":
+            fire.append([i, j])
+            f_time[i][j] = 1
+
+def bfs():
+    while fire:
+        x, y = fire.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if (nx < 0 or nx >= r or ny < 0 or ny >= c) or graph[nx][ny] == "#" or f_time[nx][ny]:
+                continue
+            f_time[nx][ny] = f_time[x][y] + 1
+            fire.append([nx, ny])
+
+    while jihoon:
+        x, y = jihoon.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= r or ny < 0 or ny >= c:
+                return j_time[x][y]
+            if graph[nx][ny] == "#" or j_time[nx][ny]:
+                continue
+            if f_time[nx][ny] and f_time[nx][ny] <= j_time[x][y] + 1:
+                continue
+            j_time[nx][ny] = j_time[x][y] + 1
+            jihoon.append([nx, ny])
+    return "IMPOSSIBLE"
+
+print(bfs())
