79b3bb59e01d3b68eb624e151d725b19f89bd89a
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2024-02-06 17:04:25 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 900 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -1,18 +1,12 @@
-import sys
-import math
-input = sys.stdin.readline
-
-def hanoi(n, start, end, sub):
-    if n == 1:
-        move(start, end)
-        return
-    hanoi(n-1, start, sub, end)
-    move(start, end)
-    hanoi(n-1, sub, end, start)
-
-def move(start, end):
-    print(start, end)
-
 n = int(input())
+
+def hanoi(start, end, n):
+    if n == 1:
+        print(start, end)
+        return
+    hanoi(start, 6-start-end, n-1)
+    print(start, end)
+    hanoi(6-start-end, end, n-1)
+
 print(2**n - 1)
-hanoi(n, 1, 3, 2)
+hanoi(1, 3, n)
