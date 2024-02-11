2e452f0867bb413c766d97a466db2f09ce879126
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2024-02-11 14:53:28 +0900

    [Gold IV] Title: N-Queen, Time: 25688 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,25 @@
+n = int(input())
+count = 0
+visited1 = [0 for _ in range(n*2)]
+visited2 = [0 for _ in range(n*2)]
+visited3 = [0 for _ in range(n*2)]
+
+def solve(k):
+    global count
+    if k == n:
+        count += 1
+        return
+
+    for i in range(n):
+        if visited1[i] or visited2[i+k] or visited3[k-i+n-1]:
+            continue
+        visited1[i] = 1
+        visited2[i+k] = 1
+        visited3[k-i+n-1] = 1
+        solve(k+1)
+        visited1[i] = 0
+        visited2[i+k] = 0
+        visited3[k-i+n-1] = 0
+
+solve(0)
+print(count)
