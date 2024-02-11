7e7d528dbcd1788f0fef1fc6c4b060508b6f277e
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2024-02-11 19:56:54 +0900

    [Silver III] Title: N과 M (7), Time: 2104 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.py
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -1,17 +1,7 @@
+from itertools import product
+
 n, m = map(int, input().split())
 arr = sorted(list(map(int, input().split())))
-tmp = [0] * n
 
-def solve(k):
-    if k == m:
-        for i in range(m):
-            print(arr[tmp[i]], end=" ")
-        print()
-        return
-
-    for i in range(n):
-        tmp[k] = i
-        solve(k+1)
-
-
-solve(0)
+for p in product(arr, repeat=m):
+    print(*p)
