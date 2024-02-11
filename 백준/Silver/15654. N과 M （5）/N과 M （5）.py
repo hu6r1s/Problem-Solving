1a761034486249df7d966e56e5c838ced0aa9141
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2024-02-11 18:51:18 +0900

    [Silver III] Title: N과 M (5), Time: 152 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -1,21 +1,6 @@
+from itertools import permutations
+
 n, m = map(int, input().split())
 arr = sorted(list(map(int, input().split())))
-visited = [0] * n
-tmp = [0] * n
-
-def solve(k):
-    if k == m:
-        for i in range(m):
-            print(arr[tmp[i]], end=" ")
-        print()
-        return
-
-    for i in range(n):
-        if not visited[i]:
-            tmp[k] = i
-            visited[i] = 1
-            solve(k+1)
-            visited[i] = 0
-
-
-solve(0)
+for p in permutations(arr, m):
+    print(*p)
