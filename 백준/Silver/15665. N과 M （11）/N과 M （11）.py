915e1a7178709d5064ff8f40b49068186c81457b
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2024-02-12 10:28:38 +0900

    [Silver II] Title: N과 M (11), Time: 1032 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15665. N과 M （11）/N과 M （11）.py
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.py
@@ -1,12 +1,21 @@
-from itertools import product
-
 n, m = map(int, input().split())
 arr = sorted(list(map(int, input().split())))
-result = set()
-for p in product(arr, repeat=m):
-    result.add(p)
+tmp = [0] * n
 
-result = sorted(list(result))
+def solve(k):
+    if k == m:
+        for i in range(m):
+            print(arr[tmp[i]], end=" ")
+        print()
+        return
 
-for r in result:
-    print(*r)
+    prev = 0
+
+    for i in range(n):
+        if prev != arr[i]:
+            prev = arr[i]
+            tmp[k] = i
+            solve(k+1)
+
+
+solve(0)
