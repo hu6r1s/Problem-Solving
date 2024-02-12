37dbe865ef01626d54207affc5b349a1655dec4e
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2024-02-12 10:32:17 +0900

    [Silver II] Title: N과 M (10), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.py
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -1,27 +1,12 @@
+from itertools import combinations
+
 n, m = map(int, input().split())
 arr = sorted(list(map(int, input().split())))
-tmp = [0] * n
-visited = [0] * n
+result = set()
+for c in combinations(arr, m):
+    result.add(c)
 
-def solve(k):
-    if k == m:
-        for i in range(m):
-            print(arr[tmp[i]], end=" ")
-        print()
-        return
+result = sorted(list(result))
 
-    prev = 0
-    si = 0
-    if k != 0:
-        si = tmp[k-1] + 1
-
-    for i in range(si, n):
-        if not visited[i] and prev != arr[i]:
-            tmp[k] = i
-            prev = arr[i]
-            visited[i] = 1
-            solve(k+1)
-            visited[i] = 0
-
-
-solve(0)
+for r in result:
+    print(*r)
