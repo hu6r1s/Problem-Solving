7cee975fe760cec68a369dd68bb17edcc5e6ecd7
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2024-02-14 09:31:11 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,15 +1,22 @@
+from collections import deque
+
 def solution(progresses, speeds):
-    result = []
-    while progresses:
+    days = deque()
+    for i in range(len(progresses)):
         count = 0
-        while progresses and progresses[0] >= 100:
-            count += 1
-            progresses.pop(0)
-            speeds.pop(0)
-        
-        for i in range(len(progresses)):
+        while progresses[i] < 100:
             progresses[i] += speeds[i]
+            count += 1
+        days.append(count)
+    
+    result = []
+    while days:
+        day = days.popleft()
+        a = 1
+        while days and day >= days[0]:
+            a += 1
+            days.popleft()
         
-        if count:
-            result.append(count)
+        result.append(a)
+        
     return result
