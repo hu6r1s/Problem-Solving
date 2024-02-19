e09b95c05b3d668f22e9e93ef0691947da501c6a
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2024-02-19 19:40:29 +0900

    [Silver II] Title: 연속합, Time: 92 ms, Memory: 38828 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,6 +1,11 @@
 n = int(input())
-m = list(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
+dp = [0] * (n+1)
+dp[1] = arr[1]
 
-for i in range(1, n):
-    m[i] = max(m[i], m[i] + m[i - 1])
-print(max(m))
+if max(arr) > 0:
+    for i in range(1, n+1):
+        dp[i] = max(0, dp[i-1] + arr[i])
+    print(max(dp))
+else:
+    print(max(arr[1:]))
