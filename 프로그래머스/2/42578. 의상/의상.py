ff6775c6247bf12347d01497ddad9b8494d2c26a
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2024-03-12 09:10:27 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,11 @@
 def solution(clothes):
     dict1 = dict()
-    for i, j in clothes:
-        if j in dict1:
-            dict1[j] += 1
+    for cloth, type in clothes:
+        if type in dict1:
+            dict1[type] += 1
         else:
-            dict1[j] = 1
+            dict1[type] = 1
     count = 1
     for i in dict1.values():
-        count *= i + 1
-    return count - 1
+        count *= i+1
+    return count-1
