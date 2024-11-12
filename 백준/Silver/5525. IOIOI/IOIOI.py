e2a80c66b142c5c25f8a0fd6fa2083fc4b6c5f57
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2024-11-13 00:22:25 +0900

    [Silver I] Title: IOIOI, Time: 32 ms, Memory: 31120 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -0,0 +1,16 @@
+n = int(input())
+m = int(input())
+p = ""
+for i in range(2 * n + 1):
+    if i % 2:
+        p += "O"
+    else:
+        p += "I"
+
+s = input()
+cnt = 0
+for i in range(0, len(s)-len(p)+1):
+    if p == s[i:len(p)+i]:
+        cnt += 1
+
+print(cnt)
