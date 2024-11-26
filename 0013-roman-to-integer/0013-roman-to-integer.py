db08ac9ad7b5243feb2c299d5b88a8d4fc0785aa
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2024-11-26 12:57:51 +0900

    Time: 15 ms (5.81%), Space: 16.7 MB (15.66%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,23 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        symbol = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        value = 0
+        for i in range(len(s)-1):
+            if symbol[s[i]] < symbol[s[i+1]]:
+                value -= symbol[s[i]]
+            else:
+                value += symbol[s[i]]
+            print(value)
+        value += symbol[s[-1]]
+        return value
+# 문자열 뒤집기
+# 처음부터 끝까지 진행하는데 다음 문자가 지금 문자보다 작으면 빼기
+# 마지막 값 더하
