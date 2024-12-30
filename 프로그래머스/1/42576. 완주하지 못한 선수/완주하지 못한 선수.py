0e6c80f1c6f54b9cf10d47cdb5130dd2eb5e7483
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2024-12-30 15:36:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 54.33 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,12 +1,14 @@
+from collections import defaultdict
+
 def solution(participant, completion):
-    dict1 = dict()
-    for i in participant:
-        if i in dict1:
-            dict1[i] += 1
-        else:
-            dict1[i] = 1
-    for i in completion:
-        dict1[i] -= 1
-    for i in dict1.keys():
-        if dict1[i] != 0:
-            return i
+    lst = defaultdict(int)
+    
+    for part in participant:
+        lst[part] += 1
+    
+    for com in completion:
+        lst[com] -= 1
+    
+    for name in lst:
+        if lst[name]:
+            return name
