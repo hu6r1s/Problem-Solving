cb65546f8722d260d3c6bed3574da5dab83cfb80
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-03-20 17:13:53 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.07 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,11 +1,10 @@
 def solution(n):
-    result = 0
-    i = 1
-    while True:
-        if i == n:
-            result += 1
-            break
-        if not n % i:
-            result += 1
-        i += 1
-    return result
+    answer = 0
+    for i in range(1, int(n ** 0.5) + 1):
+        if n % i == 0:
+            answer += 2
+
+            if i * i == n:
+                answer -= 1
+
+    return answer
