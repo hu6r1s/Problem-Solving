07e2ce69af03df6d7cb0b7ec6e78aa5fdc2287ce
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-03-23 13:30:54 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 9.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,2 @@
+def solution(a, b, c):
+    return (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3) if a == b == c else (a + b + c) * (a ** 2 + b ** 2 + c ** 2) if (a == b and b != c) or (b == c and a != c) or (a == c and a != b) else a + b + c
