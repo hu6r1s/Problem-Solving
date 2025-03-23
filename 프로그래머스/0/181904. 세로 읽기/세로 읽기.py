1356c78584ec0edaaea12573c50d26b138bddd0a
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-03-23 17:45:24 +0900

    [level 0] Title: 세로 읽기, Time: 0.00 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.py
@@ -1,5 +1,2 @@
 def solution(my_string, m, c):
-    string = []
-    for i in range(0, len(my_string), m):
-        string.append(list(my_string[i:i+m]))
-    return "".join(list(zip(*string))[c-1])
+    return my_string[c-1::m]
