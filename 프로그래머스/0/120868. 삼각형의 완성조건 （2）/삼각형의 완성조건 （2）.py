e29caa233be64f2b35c5791c009daa7066fb23c9
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-04-02 11:46:31 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,12 +1,2 @@
 def solution(sides):
-    sides.sort()
-    max_side = []
-    max_anon = []
-    for side in range(1, sides[-1]+1):
-        if sides[0] + side > sides[-1]:
-            max_side.append(side)
-            
-    for side in range(sides[-1], sum(sides)):
-        if sides[-1] < side:
-            max_anon.append(side)
-    return len(max_side) + len(max_anon)
+    return 2 * min(sides) - 1
