20c3ab39f38aac49ed2f8efb07144867d9045f10
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-04-02 13:04:53 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 1.52 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.py
@@ -1,7 +1,10 @@
 def solution(arr, k):
-    result = []
-    for i in range(len(arr)):
-        if arr[i] in result:
-            continue
-        result.append(arr[i])
-    return result[:k] if len(result) >= k else result + [-1] * (k - len(result))
+    # result = []
+    # for i in range(len(arr)):
+    #     if arr[i] in result:
+    #         continue
+    #     result.append(arr[i])
+    # return result[:k] if len(result) >= k else result + [-1] * (k - len(result))
+    result = list(dict.fromkeys(arr))
+    result.extend([-1] * max(0, k - len(result)))
+    return result[:k]
