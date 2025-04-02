6c38ad15bc4598ae03f4b89064a0303345ce68d1
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-04-02 10:59:08 +0900

    [level 0] Title: 공 던지기, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.py
@@ -1,3 +1,7 @@
+from collections import deque
+
 def solution(numbers, k):
-    return numbers[2 * (k - 1) % len(numbers)]
+    arr = deque(numbers)
+    arr.rotate(-(2 * (k - 1)))
+    return arr[0]
     
