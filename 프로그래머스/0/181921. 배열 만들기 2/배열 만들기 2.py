cd94a2708d66d29faf9074a00b3e2e4a744ac489
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-04-05 12:11:33 +0900

    [level 0] Title: 배열 만들기 2, Time: 664.63 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,6 +1,6 @@
 def solution(l, r):
     result = []
     for i in range(l, r+1):
-        if all(ch in ['0', '5'] for ch in str(i)):
+        if not set(list(str(i))) - {'0', '5'}:
             result.append(i)
     return result if result else [-1]
