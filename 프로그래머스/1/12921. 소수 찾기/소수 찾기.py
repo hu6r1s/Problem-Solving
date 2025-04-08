6dbbc3c2f2f1123c703400e00c310883a9d0f8df
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-04-08 14:02:06 +0900

    [level 1] Title: 소수 찾기, Time: 351.86 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,13 +1,7 @@
-def is_prime(n):
-    for i in range(2, int(n ** 0.5) + 1):
-        if not n % i:
-            return False
-    return True
-
-
 def solution(n):
-    result = 0
-    for i in range(2, n+1):
-        if is_prime(i):
-            result += 1
-    return result
+    num=set(range(2,n+1))
+
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
