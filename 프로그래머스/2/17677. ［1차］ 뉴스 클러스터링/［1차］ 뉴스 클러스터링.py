ea2388cc002a9b4065b3ff631e0be0ccc8abcb4f
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-04-15 10:49:25 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.45 ms, Memory: 9.41 MB -BaekjoonHub

---
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+
+def solution(str1, str2):
+    str1, str2 = str1.lower(), str2.lower()
+    cnt_str1, cnt_str2 = [], []
+    for i in range(len(str1)-1):
+        if str1[i:i+2].isalpha():
+            cnt_str1.append(str1[i:i+2])
+    for i in range(len(str2)-1):
+        if str2[i:i+2].isalpha():
+            cnt_str2.append(str2[i:i+2])
+    
+    gyo = sum((Counter(cnt_str1) & Counter(cnt_str2)).values())
+    hap = sum((Counter(cnt_str1) | Counter(cnt_str2)).values())
+    jacard = 1 if not hap else gyo / hap
+    return int(jacard * 65536)
