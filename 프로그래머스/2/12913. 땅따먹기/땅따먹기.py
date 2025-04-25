fe7eb523c37226badfbdaa1376887cae0e20465d
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-04-25 17:12:07 +0900

    [level 2] Title: 땅따먹기, Time: 479.92 ms, Memory: 42.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -0,0 +1,17 @@
+def solution(land):
+    dp = [[0] * 4 for _ in range(len(land))]
+    dp[0] = land[0]
+    
+    for i in range(1, len(land)):
+        for j in range(4):
+            dp[i][j] = max(dp[i-1][k] + land[i][j] for k in range(4) if j != k)
+    return max(dp[-1])
+    
+    
+    
+    
+# dp로 풀이
+# 1로 갔을 때 1 + 6, 1 + 7, 1 + 8 
+# 2로 갔을 때 2 + 5, 2 + 7, 2 + 8 
+# 3로 갔을 때 3 + 5, 3 + 6, 3 + 8 
+# 5로 갔을 때 
