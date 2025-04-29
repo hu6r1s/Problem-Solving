3547cbea8a27aa013c3691951482e3f09e7f3995
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-04-29 22:57:54 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.10 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.py
@@ -1,8 +1,12 @@
 def solution(lines):
-    visited = [0] * 201
-    cnt = 0
+#     visited = [0] * 201
     
-    for x1, x2 in lines:
-        for i in range(x1 + 100, x2 + 100):
-            visited[i] += 1
-    return sum([1 for v in visited if v >= 2])
+#     for x1, x2 in lines:
+#         for i in range(x1 + 100, x2 + 100):
+#             visited[i] += 1
+#     return sum([1 for v in visited if v >= 2])
+    
+    s1 = set(i for i in range(lines[0][0], lines[0][1]))
+    s2 = set(i for i in range(lines[1][0], lines[1][1]))
+    s3 = set(i for i in range(lines[2][0], lines[2][1]))
+    return len((s1 & s2) | (s2 & s3) | (s1 & s3))
