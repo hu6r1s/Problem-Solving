fd5d577acada7f1c342fd3ce6c41b0bd6c7ffc89
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-06-18 13:12:33 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.04 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        if (is_prefix.length() > my_string.length()) {
-            return 0;
-        }
-        if (my_string.substring(0, is_prefix.length()).equals(is_prefix)) {
+        if (my_string.startsWith(is_prefix)) {
             return 1;
         } else {
             return 0;
