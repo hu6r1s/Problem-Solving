2c860f378047c38f95f2fca07078b809e9dc630a
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-07-20 20:27:22 +0900

    Time: 0 ms (100%), Space: 17.7 MB (82.72%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -2,7 +2,8 @@ class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         k = 0
         for i in range(len(nums)):
-            if nums[i] != val:
-                nums[k] = nums[i]
+            if nums[i] == val:
+                nums[i] = 1e9
                 k += 1
-        return k
+        nums.sort()
+        return len(nums) - k
