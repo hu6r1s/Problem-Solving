1c0fced642465112f1a7807d124fe80c8a6ed01f
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-07-22 11:32:45 +0900

    Time: 52 ms (52.84%), Space: 34.3 MB (52.71%) - LeetHub

---
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -0,0 +1,22 @@
+class Solution:
+    """
+        1. nums를 내림차순 sort하고, 차례대로 두 개의 차가 다음 차가 같다면 + 1
+        처음 차는 diff 변수에 저장하고, 진행되면서 diff를 업데이트
+        200, 100, 4, 3, 2, 1
+
+        연속된 수열이라고 하여 1 이외의 다른 수가 올 수 있을 것이라고 판단하였지만 차이가 1로 나온다는 것을 확인
+
+    """
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        longest = 0
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                k = 1
+            
+                while num + k in num_set:
+                    k += 1
+                
+                longest = max(longest, k)
+        return longest
