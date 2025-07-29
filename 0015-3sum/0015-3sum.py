dd109831f1735237becc739daeb79afacdd72330
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-07-29 11:35:03 +0900

    Time: 601 ms (40.77%), Space: 20.8 MB (36.48%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,34 @@
+class Solution:
+    """
+        1. 3 way for문으로 돌아가면서 0인 합을 찾는 방법
+            - O(n^3)으로 시간초과
+        2. 
+    """
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            left, right = i + 1, len(nums) - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+
+        return result
