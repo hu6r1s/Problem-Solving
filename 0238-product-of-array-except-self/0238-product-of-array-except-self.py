bfe15cd68862c4ba3a255a01362ecec3382c9bdc
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-07-30 18:33:50 +0900

    Time: 20 ms (74.61%), Space: 23.3 MB (79.77%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -5,7 +5,8 @@ class Solution:
         1. 인덱스 슬라이싱을 활용한 풀이
             - 인덱스 슬라이싱을 활용하여 해당 인덱스를 제외한 나머지 원소들의 곲을 math.prod()를 이용하여 배열에 삽입
             - 시간초과 O(n^2)
-        2. 
+        2. DP Bottom Up 방식으로 풀이
+            - 
     """
     # 1번 풀이
     # def productExceptSelf(self, nums: List[int]) -> List[int]:
