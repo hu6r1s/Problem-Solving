508f62b42f3cc4b586b4a3d9879a8397fd66b7e7
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-07-31 20:36:55 +0900

    [Bronze III] Title: 다음수, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4880. 다음수/다음수.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    a1, a2, a3 = map(int, input().split())
+
+    if a1 == 0 and a2 == 0 and a3 == 0:
+        break
+
+    if a3 - a2 == a2 - a1:
+        print(f"AP {a3 + (a3 - a2)}")
+    elif a3 // a2 == a2 // a1:
+        print(f"GP {a3 * (a3 // a2)}")
