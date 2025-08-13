61bbc818a936fffc342d1209aa965883e9423de1
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-08-13 20:50:59 +0900

    Time: 0 ms (100%), Space: 18 MB (80.08%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -26,14 +26,32 @@ class Solution:
                 return nums[0]
     """
 
+    """
+    2. 요소별로 이전 값과 현재 값을 비교하여 작은 값이 나오면 그 값이 첫번쨰 요소, 즉 가장 작은 요소가 됨
+    시간복잡도: O(n)
+    - 최악의 경우 배열 전체를 한 번 순회해야 함
+    - n = len(nums)
+    공간복잡도: O(1)
+    - 추가로 사용하는 변수는 i와 몇 개의 상수형 변수뿐
+    """
+    """
     def findMin(self, nums: List[int]) -> int:
         for i in range(1, len(nums)):
             if nums[i - 1] > nums[i]:
                 return nums[i]
         return nums[0]
-"""
-
-
-2. 요소별로 이전 값과 현재 값을 비교하여 작은 값이 나오면 그 값이 첫번쨰 요소, 즉 가장 작은 요소가 됨
-
-"""
+    """
+    """
+    3. 이분탐색을 사용하는 방법
+    """
+    def findMin(self, nums: List[int]) -> int:
+        start, end = 1, len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid - 1] > nums[mid]:
+                return nums[mid]
+            if nums[0] < nums[mid]:
+                start = mid + 1
+            else:
+                end = mid - 1
+        return nums[0]
