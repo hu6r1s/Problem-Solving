94fbdfb5b334b9117f0a6ae8f727d25f2e73ce3c
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-08-20 16:40:01 +0900

    Time: 16 ms (42%), Space: 21.2 MB (47.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,9 @@
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dict_strs = {}
+        dict_strs = defaultdict(list)
         
         for word in strs:
-            if not str(sorted(word)) in dict_strs:
-                dict_strs[str(sorted(word))] = []
             dict_strs[str(sorted(word))].append(word)
         return list(dict_strs.values())
