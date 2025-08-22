fa8e2085217dc71aeee473c72201609ab04a148d
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-08-22 17:40:18 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 96.91 ms, Memory: 96.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String part : participant) {
+            map.put(part, map.getOrDefault(part, 0) + 1);
+        }
+        
+        for (String com : completion) {
+            map.put(com, map.get(com) - 1);
+        }
+        
+        for (String key : map.keySet()) {
+            if (map.get(key) > 0) {
+                return key;
+            }
+        }
+        return "";
+    }
+}
