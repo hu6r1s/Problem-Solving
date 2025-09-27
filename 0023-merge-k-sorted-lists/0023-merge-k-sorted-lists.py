f388f13884ab9afd62bdb3d4f2a5891bd4572d13
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-09-27 13:40:28 +0900

    Time: 11 ms (58.89%), Space: 20.3 MB (54.61%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,12 +3,29 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import heapq
 class Solution:
+    # def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    #     dummy = curr = ListNode()
+    #     while any(lists):
+    #         val, idx = min((li.val, idx) for idx, li in enumerate(lists) if li)
+    #         curr.next = ListNode(val)
+    #         curr = curr.next
+    #         lists[idx] = lists[idx].next
+    #     return dummy.next
+
+
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        heap = [(li.val, idx) for idx, li in enumerate(lists) if li]
+        heapq.heapify(heap)
+
         dummy = curr = ListNode()
-        while any(lists):
-            val, idx = min((li.val, idx) for idx, li in enumerate(lists) if li)
+        while heap:
+            val, idx = heapq.heappop(heap)
             curr.next = ListNode(val)
             curr = curr.next
+
             lists[idx] = lists[idx].next
+            if lists[idx]:
+                heapq.heappush(heap, (lists[idx].val, idx))
         return dummy.next
