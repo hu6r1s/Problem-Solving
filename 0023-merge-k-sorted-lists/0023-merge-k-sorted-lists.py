a64d7424516dbee99b3021024cc0bcd7cd84ef45
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-09-27 13:43:41 +0900

    Time: 7 ms (89.08%), Space: 20.3 MB (54.61%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -5,6 +5,9 @@
 #         self.next = next
 import heapq
 class Solution:
+    """
+    브루트포스
+    """
     # def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
     #     dummy = curr = ListNode()
     #     while any(lists):
@@ -14,18 +17,50 @@ class Solution:
     #         lists[idx] = lists[idx].next
     #     return dummy.next
 
+    """
+    최소힙 활용
+    """
+    # def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    #     heap = [(li.val, idx) for idx, li in enumerate(lists) if li]
+    #     heapq.heapify(heap)
+
+    #     dummy = curr = ListNode()
+    #     while heap:
+    #         val, idx = heapq.heappop(heap)
+    #         curr.next = ListNode(val)
+    #         curr = curr.next
+
+    #         lists[idx] = lists[idx].next
+    #         if lists[idx]:
+    #             heapq.heappush(heap, (lists[idx].val, idx))
+    #     return dummy.next
 
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        heap = [(li.val, idx) for idx, li in enumerate(lists) if li]
-        heapq.heapify(heap)
+        def mergeTwoLists(li1, li2):
+            dummy = node = ListNode(-1)
+            while li1 and li2:
+                if li1.val < li2.val:
+                    node.next = li1
+                    li1 = li1.next
+                else:
+                    node.next = li2
+                    li2 = li2.next
+                node = node.next
+            node.next = li1 if li1 else li2
+            return dummy.next
 
-        dummy = curr = ListNode()
-        while heap:
-            val, idx = heapq.heappop(heap)
-            curr.next = ListNode(val)
-            curr = curr.next
+        if len(lists) == 0:
+            return None
 
-            lists[idx] = lists[idx].next
-            if lists[idx]:
-                heapq.heappush(heap, (lists[idx].val, idx))
-        return dummy.next
+        def dfs(low, high):
+            if low == high:
+                return lists[low]
+            if low + 1 == high:
+                return mergeTwoLists(lists[low], lists[high])
+
+            mid = (low + high) // 2
+            li1 = dfs(low, mid)
+            li2 = dfs(mid + 1, high)
+            return mergeTwoLists(li1, li2)
+
+        return dfs(0, len(lists) - 1)
