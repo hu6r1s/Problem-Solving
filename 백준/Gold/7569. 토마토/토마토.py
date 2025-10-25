6f1dda2cea2db4b5ab5c100c1d07d752fe63baf1
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-10-25 17:04:46 +0900

    [Gold V] Title: 토마토, Time: 2124 ms, Memory: 53392 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,5 +1,16 @@
 from collections import deque
 
+m, n, h = map(int, input().split())
+dx, dy, dz = [0, 0, 0, 0, -1, 1], [0, 0, 1, -1, 0, 0], [1, -1, 0, 0, 0, 0]
+board = [[list(map(int, (input().split()))) for _ in range(n)] for _ in range(h)]
+queue = deque()
+
+for z in range(h):
+    for y in range(n):
+        for x in range(m):
+            if board[z][y][x] == 1:
+                queue.append([x, y, z])
+
 def bfs():
     while queue:
         x, y, z = queue.popleft()
@@ -7,35 +18,24 @@ def bfs():
             nx = x + dx[i]
             ny = y + dy[i]
             nz = z + dz[i]
-            if nx < 0 or nx >= k or ny < 0 or ny >= n or nz < 0 or nz >= m:
+            if nx < 0 or nx >= m or ny < 0 or ny >= n or nz < 0 or nz >= h:
                 continue
-            if not graph[nx][ny][nz] and not visited[nx][ny][nz]:
-                graph[nx][ny][nz] = graph[x][y][z] + 1
-                visited[nx][ny][nz] = 1
-                queue.append([nx, ny, nz])
+            if board[nz][ny][nx] != 0:
+                continue
+
+            board[nz][ny][nx] = board[z][y][x] + 1
+            queue.append([nx, ny, nz])
 
 
-m, n, k = map(int, input().split())
-graph = [[list(map(int, input().split())) for _ in range(n)] for _ in range(k)]
-dx = [0, 0, -1, 1, 0, 0]
-dy = [1, -1, 0, 0, 0, 0]
-dz = [0, 0, 0, 0, 1, -1]
-visited = [[[0]*m for _ in range(n)] for _ in range(k)]
-queue = deque()
-for i in range(k):
-    for j in range(n):
-        for u in range(m):
-            if graph[i][j][u] == 1 and not visited[i][j][u]:
-                queue.append([i, j, u])
-                visited[i][j][u] = 1
 bfs()
+count = 0
 
-result = 0
-for g in graph:
-    for i in g:
-        for j in i:
-            if j == 0:
+for z in range(h):
+    for y in range(n):
+        for x in range(m):
+            if board[z][y][x] == 0:
                 print(-1)
-                exit(0)
-        result = max(result, max(i))
-print(result-1)
+                exit()
+            count = max(count, board[z][y][x])
+
+print(count - 1)
