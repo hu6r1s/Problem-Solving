550b7fac99aef8ab0350bf9729eac2235f0b4822
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-11-12 17:02:16 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34936 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,15 +1,12 @@
 from collections import deque
 
 n, m = map(int, input().split())
-graph = [list(map(int, input())) for _ in range(n)]
-visited = [[0]*m for _ in range(n)]
-count = 1
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-def bfs(x, y):
+dx, dy = [-1, 1, 0, 0], [0, 0, 1, -1]
+board = [list(map(int, list(input()))) for _ in range(n)]
+
+def bfs(a, b):
     queue = deque()
-    queue.append([x, y])
-    visited[x][y] = 1
+    queue.append([a, b])
     while queue:
         x, y = queue.popleft()
         for i in range(4):
@@ -17,8 +14,10 @@ def bfs(x, y):
             ny = y + dy[i]
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-            if graph[nx][ny] == 1 and visited[nx][ny] == 0:
-                visited[nx][ny] = visited[x][y] + 1
-                queue.append([nx, ny])
+            if board[nx][ny] == 0 or board[nx][ny] > 1:
+                continue
+            board[nx][ny] = board[x][y] + 1
+            queue.append([nx, ny])
+
 bfs(0, 0)
-print(visited[-1][-1])
+print(board[n-1][m-1])
