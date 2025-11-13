ecba32f99947f032cc43703eacf4aba8c2b3f569
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-11-13 18:34:52 +0900

    [Gold IV] Title: 불, Time: 2568 ms, Memory: 117508 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.py
+++ 백준/Gold/5427. 불/불.py
@@ -1,54 +1,52 @@
 from collections import deque
 
 t = int(input())
-for _ in range(t):
-    w, h = map(int, input().split())
-    graph = [list(input()) for _ in range(h)]
-    dx = [0, 0, -1, 1]
-    dy = [-1, 1, 0, 0]
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 
-    fire = deque()
-    sang = deque()
-    f_time = [[0]*w for _ in range(h)]
-    s_time = [[0]*w for _ in range(h)]
-    for i in range(h):
-        for j in range(w):
-            if graph[i][j] == "*":
-                fire.append([i, j])
-                f_time[i][j] = 1
-            elif graph[i][j] == "@":
-                sang.append([i, j])
-                s_time[i][j] = 1
+def bfs():
+    while fire:
+        x, y = fire.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= h or ny < 0 or ny >= w:
+                continue
+            if board[nx][ny] == "#" or fire_time[nx][ny] != 0:
+                continue
+            fire_time[nx][ny] = fire_time[x][y] + 1
+            fire.append([nx, ny])
 
-    def f_bfs():
-        while fire:
-            x, y = fire.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if nx < 0 or nx >= h or ny < 0 or ny >= w:
-                    continue
-                if graph[nx][ny] == "#" or f_time[nx][ny]:
-                    continue
-
-                f_time[nx][ny] = f_time[x][y] + 1
-                fire.append([nx, ny])
-
-    def s_bfs():
-        while sang:
-            x, y = sang.popleft()
-            for i in range(4):
-                nx = x + dx[i]
-                ny = y + dy[i]
-                if nx < 0 or nx >= h or ny < 0 or ny >= w:
-                    return s_time[x][y]
-                if graph[nx][ny] == "#" or s_time[nx][ny]:
-                    continue
-                if not f_time[nx][ny] or f_time[nx][ny] > s_time[x][y] + 1:
-                    s_time[nx][ny] = s_time[x][y] + 1
-                    sang.append([nx, ny])
+    while sanggn:
+        x, y = sanggn.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= h or ny < 0 or ny >= w:
+                return sanggn_time[x][y]
+            if board[nx][ny] == "#" or sanggn_time[nx][ny] != 0:
+                continue
+            if fire_time[nx][ny] != 0 and fire_time[nx][ny] <= sanggn_time[x][y] + 1:
+                continue
+            sanggn_time[nx][ny] = sanggn_time[x][y] + 1
+            sanggn.append([nx, ny])
+    else:
         return "IMPOSSIBLE"
 
+for _ in range(t):
+    w, h = map(int, input().split())
+    board = [list(input()) for _ in range(h)]
+    fire_time = [[0] * w for _ in range(h)]
+    fire = deque()
+    sanggn = deque()
+    sanggn_time = [[0] * w for _ in range(h)]
 
-    f_bfs()
-    print(s_bfs())
+    for i in range(h):
+        for j in range(w):
+            if board[i][j] == "*":
+                fire_time[i][j] = 1
+                fire.append([i, j])
+            if board[i][j] == "@":
+                sanggn.append([i, j])
+                sanggn_time[i][j] = 1
+
+    print(bfs())
