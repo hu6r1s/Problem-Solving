67f9d917e3c4def652f40b9ebe8fdd1d24816d48
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-11-13 11:38:18 +0900

    [Gold V] Title: 토마토, Time: 1384 ms, Memory: 108664 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.py
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -1,12 +1,14 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
 
 m, n = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-visited = [[False]*m for _ in range(n)]
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+dx, dy = [-1, 1, 0, 0], [0, 0, 1, -1]
+board = [list(map(int, input().split())) for _ in range(n)]
+queue = deque()
+
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 1:
+            queue.append([i, j])
 
 def bfs():
     while queue:
@@ -16,24 +18,18 @@ def bfs():
             ny = y + dy[i]
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-            if graph[nx][ny] == 0 and visited[nx][ny] == False:
-                graph[nx][ny] = graph[x][y] + 1
-                visited[nx][ny] = True
-                queue.append([nx, ny])
+            if board[nx][ny] > 0 or board[nx][ny] == -1:
+                continue
+            board[nx][ny] = board[x][y] + 1
+            queue.append([nx, ny])
 
-queue = deque()
+bfs()
 
+result = 0
 for i in range(n):
     for j in range(m):
-        if graph[i][j] == 1:
-            queue.append([i, j])
-            visited[i][j] = True
-bfs()
-result = 0
-for g in graph:
-    if 0 in g:
-        result = -1
-        break
-    if result <= max(g)-1:
-        result = max(g)-1
-print(result)
+        if board[i][j] == 0:
+            print(-1)
+            exit()
+        result = max(result, board[i][j])
+print(result-1)
