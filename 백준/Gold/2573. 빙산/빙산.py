91cb2592166860e0bb923ba6dc9edda78b33a272
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-11-24 15:30:54 +0900

    [Gold IV] Title: 빙산, Time: 2924 ms, Memory: 36896 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,49 +1,67 @@
 from collections import deque
 
-def bfs(x, y):
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+year = 1
+icebergs = [(i, j) for i in range(n) for j in range(m) if board[i][j] > 0]
+
+def melt(icebergs):
+    melt_list = []
+    for x, y in icebergs:
+        cnt = 0
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 0:
+                cnt += 1
+        melt_list.append((x, y, cnt))
+    return melt_list
+
+
+def bfs(a, b):
     queue = deque()
-    queue.append([x, y])
-    visited[x][y] = 1
-    iceList = []
+    queue.append((a, b))
+    visited[a][b] = True
     while queue:
         x, y = queue.popleft()
-        ice = 0
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-            if not graph[nx][ny]:
-                ice += 1
-            if not graph[nx][ny] or visited[nx][ny]:
+            if visited[nx][ny] or board[nx][ny] <= 0:
                 continue
-            queue.append([nx, ny])
-            visited[nx][ny] = 1
-        if ice > 0:
-            iceList.append([x, y, ice])
-
-    for x, y, ice in iceList:
-        graph[x][y] = max(0, graph[x][y]-ice)
+            queue.append((nx, ny))
+            visited[nx][ny] = True
 
 
-
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-time = 0
 while True:
-    s = 0
-    visited = [[0]*m for _ in range(n)]
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] and not visited[i][j]:
-                bfs(i, j)
-                s += 1
-    time += 1
-    if s == 0:
+    melt_info = melt(icebergs)
+    new_icebergs = []
+    for x, y, cnt in melt_info:
+        board[x][y] = max(0, board[x][y] - cnt)
+        if board[x][y] > 0:
+            new_icebergs.append((x, y))
+
+    icebergs = new_icebergs
+    visited = [[False] * m for _ in range(n)]
+    cnt = 0
+    for x, y in icebergs:
+        if board[x][y] > 0 and not visited[x][y]:
+            bfs(x, y)
+            cnt += 1
+
+        if cnt >= 2:
+            print(year)
+            exit()
+
+    if cnt == 0:
         print(0)
         exit()
-    if s >= 2:
-        break
-print(time-1)
+
+    year += 1
+"""
+1. 빙산 녹이기
+2. 덩어리 개수 세기
+"""
