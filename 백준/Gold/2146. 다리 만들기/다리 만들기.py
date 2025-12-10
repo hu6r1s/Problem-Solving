7cf6b986921d9ff911c281121150fb003385a012
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2025-12-10 17:08:03 +0900

    [Gold III] Title: 다리 만들기, Time: 96 ms, Memory: 35052 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -1,58 +1,54 @@
 from collections import deque
 
-
-def i_num(x, y):
-    queue = deque()
-    queue.append([x, y])
-    visited[x][y] = 1
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-            if not visited[nx][ny] and graph[nx][ny]:
-                visited[nx][ny] = 1
-                graph[nx][ny] = num
-                queue.append([nx, ny])
-
-
-def bfs(v):
-    visited = [[0]*n for _ in range(n)]
-    queue = deque()
-    for i in range(n):
-        for j in range(n):
-            if graph[i][j] == v:
-                queue.append([i, j])
-                visited[i][j] = 1
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-            if graph[nx][ny] and graph[nx][ny] != v:
-                return visited[x][y]
-            if not graph[nx][ny] and not visited[nx][ny]:
-                visited[nx][ny] = visited[x][y] + 1
-                queue.append([nx, ny])
-
-
 n = int(input())
-graph = [list(map(int, input().split())) for _ in range(n)]
-visited = [[0]*n for _ in range(n)]
-num = 1
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+board = [list(map(int, input().split())) for _ in range(n)]
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+
+
+def bfs(a, b, c):
+    queue = deque()
+    queue.append((a, b))
+    board[a][b] = c
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
+            if board[nx][ny] != 1:
+                continue
+            board[nx][ny] = c
+            queue.append((nx, ny))
+c = 2
 for i in range(n):
     for j in range(n):
-        if not visited[i][j] and graph[i][j]:
-            graph[i][j] = num
-            i_num(i, j)
-            num += 1
-result = 10**9
-for i in range(1, num):
-    result = min(result, bfs(i))
-print(result-1)
+        if board[i][j] == 1:
+            bfs(i, j, c)
+            c += 1
+
+queue = deque()
+dist = [[-1] * n for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if board[i][j] != 0:
+            dist[i][j] = 0
+            queue.append((i, j))
+
+ans = float("inf")
+
+while queue:
+    x, y = queue.popleft()
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or nx >= n or ny < 0 or ny >= n:
+            continue
+        if board[nx][ny] == 0:
+            board[nx][ny] = board[x][y]
+            dist[nx][ny] = dist[x][y] + 1
+            queue.append((nx, ny))
+        elif board[nx][ny] != board[x][y]:
+            ans = min(ans, dist[x][y] + dist[nx][ny])
+print(ans)
