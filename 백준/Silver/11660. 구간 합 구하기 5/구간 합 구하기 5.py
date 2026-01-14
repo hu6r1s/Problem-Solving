95d2351996b3702524a639f33086bd56437639b6
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2026-01-14 11:28:03 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1016 ms, Memory: 106832 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -2,13 +2,27 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-graph = [[0] * n] + [[0] + list(map(int, input().split())) for _ in range(n)]
-dp = [[0] * (n+1) for _ in range(n+1)]
+lst = [list(map(int, input().split())) for _ in range(n)]
+s = [[0] * (n+1) for _ in range(n+1)]
 
 for i in range(1, n+1):
     for j in range(1, n+1):
-        dp[i][j] = dp[i][j-1] + dp[i-1][j] - dp[i-1][j-1] + graph[i][j]
+        s[i][j] = s[i][j-1] + s[i-1][j] - s[i-1][j-1] + lst[i-1][j-1]
 
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    print(dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1])
+    print(s[x2][y2] - s[x1-1][y2] - s[x2][y1-1] + s[x1-1][y1-1])
+
+
+"""
+1 2 3 4
+2 3 4 5
+3 4 5 6
+4 5 6 7
+
+0 0 0 0 0
+0 1 3 6 10
+0 3 8 15 24
+0 6 15 27 42
+0 10 24 42 64
+"""
