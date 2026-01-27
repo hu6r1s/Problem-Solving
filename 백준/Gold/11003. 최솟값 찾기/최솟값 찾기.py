f8f9816fb658c407db7990ed4dec0eea5e73445e
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2026-01-27 14:00:22 +0900

    [Gold I] Title: 최솟값 찾기, Time: 7168 ms, Memory: 757084 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
@@ -1,38 +1,13 @@
-# n, l = map(int, input().split())
-# a = list(map(int, input().split()))
-# min_num = []
-#
-# idx = 0
-# while idx < n:
-#     if idx - l + 1 < 0:
-#         min_num.append(min(a[:idx+1]))
-#     else:
-#         min_num.append(min(a[idx - l + 1: idx+1]))
-#     idx += 1
-#
-# print(*min_num)
-
 from collections import deque
 
 n, l = map(int, input().split())
-a = list(map(int, input().split()))
 queue = deque()
-result = []
+a = list(map(int, input().split()))
 
 for i in range(n):
     while queue and queue[-1][1] > a[i]:
         queue.pop()
-    queue.append([i, a[i]])
+    queue.append((i, a[i]))
     if queue[0][0] <= i - l:
         queue.popleft()
-    result.append(queue[0][1])
-    
-print(*result)
-
-"""
-1 5 2 3 6 2 3 7 3 5 2 6
-
-d(1) = a(-1) ~ a(1)
-d(2) = a(0) ~ a(2)
-d(3) = a(1) ~ a(3)
-"""
+    print(queue[0][1], end=" ")
