c6ef26d70acbad001262462873fbb655dcae4136
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2026-02-04 19:07:38 +0900

    [Silver V] Title: 소트인사이드, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,4 +1,15 @@
-n = list(input())
-li = list(map(int, n))
-li.sort(reverse=True)
-print("".join(map(str, li)))
+"""
+선택정렬 활용 풀이
+선택 정렬은 최댓값 또는 최솟값을 찾아 가장 앞에 있는 데이터와 swap하는 것
+"""
+import sys
+input = sys.stdin.readline
+
+lst = list(input())
+for i in range(len(lst)-1):
+    max_idx = i
+    for j in range(i+1, len(lst)):
+        if lst[j] > lst[max_idx]:
+            max_idx = j
+    lst[max_idx], lst[i] = lst[i], lst[max_idx]
+print("".join(lst))
