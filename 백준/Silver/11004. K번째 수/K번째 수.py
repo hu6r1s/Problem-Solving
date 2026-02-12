71e0fbd53b693c1706eadd3683c718f39cf2dd9b
Jung Hyeon Jun <67190090+hu6r1s@users.noreply.github.com>
2026-02-12 13:04:34 +0900

    [Silver V] Title: K번째 수, Time: 2768 ms, Memory: 624072 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+print(a[k-1])
+
+# def quick_sort(arr, start, end):
+#     if start >= end:
+#         return
+#     pivot = start
+#     left = start + 1
+#     right = end
+# 
+#     while left <= right:
+#         while left <= end and arr[left] <= arr[pivot]:
+#             left += 1
+#         while right > start and arr[right] >= arr[pivot]:
+#             right -= 1
+#         if left > right:
+#             arr[right], arr[pivot] = arr[pivot], arr[right]
+#         else:
+#             arr[left], arr[right] = arr[right], arr[left]
+# 
+#     quick_sort(arr, start, right - 1)
+#     quick_sort(arr, right + 1, end)
+# 
+# 
+# quick_sort(a, 0, n - 1)
+# print(a[k-1])
